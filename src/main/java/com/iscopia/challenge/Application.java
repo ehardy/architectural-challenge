28253d0ca2ef80a0a5cf737460b15b542d03c17e
Etienne Hardy <ehardy@iscopia.com>
2012-03-11 21:20:40 -0400

    Removed an unused method.

--- src/main/java/com/iscopia/challenge/Application.java
+++ src/main/java/com/iscopia/challenge/Application.java
@@ -1,12 +1,5 @@
 package com.iscopia.challenge;
 
-import com.google.common.io.Files;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.Charset;
-
 /**
  * @author ehardy
  */
@@ -16,21 +9,6 @@ public class Application {
 
     public Application(Transformer transformer) {
         this.transformer = transformer;
-    }
-
-    public void process(String inputFile, String outputFile) {
-        try {
-            File input = new File(getClass().getResource(inputFile).toURI());
-            String content = Files.toString(input, Charset.forName("UTF-8"));
-
-            Files.write(content, new File(System.getProperty("user.dir"), outputFile), Charset.forName("UTF-8"));
-
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
 
     public void process(ContentSource source, TransformationOutput output) {
